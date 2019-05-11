5f8fce1be73fbd203de242a77aa0d72e51dc178a
Raphaël Thériault <raphael_theriault@outlook.com>
2019-05-10 22:20:37 -0400

    Updates

---
+++ precache-manifest.5f077eaf221477f3b9085c9af19ea887.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34291e706dc5e8eb11e163001363828",
+    "url": "/adoptopenjdk-browser/index.html"
+  },
+  {
+    "revision": "81631d0f4f15a6dd40b9",
+    "url": "/adoptopenjdk-browser/static/js/2.d2a134d2.chunk.js"
+  },
+  {
+    "revision": "66ca53a671c5c2c675f0",
+    "url": "/adoptopenjdk-browser/static/js/main.cc68ed3d.chunk.js"
+  },
+  {
+    "revision": "7a294f7b7b9c0f3dcc8c",
+    "url": "/adoptopenjdk-browser/static/js/runtime~main.1f5200f7.js"
+  }
+]);
